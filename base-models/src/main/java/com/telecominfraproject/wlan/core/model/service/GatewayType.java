ae8ef9e76b3e023f19f36569cc9674997ca619e7
Dmitry Toptygin <dmitry.toptygin@connectus.ai>
2020-05-11 18:31:43 -0400

    renamed CloudServiceType -> GatewayType

--- base-models/src/main/java/com/telecominfraproject/wlan/core/model/service/GatewayType.java
+++ base-models/src/main/java/com/telecominfraproject/wlan/core/model/service/GatewayType.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.telecominfraproject.wlan.core.model.json.JsonDeserializationUtils;
-import com.telecominfraproject.wlan.core.model.service.CloudServiceType;
+import com.telecominfraproject.wlan.core.model.service.GatewayType;
 
 /**
  * Different type of Cloud Service
@@ -16,7 +16,7 @@ import com.telecominfraproject.wlan.core.model.service.CloudServiceType;
  * @author yongli
  *
  */
-public enum CloudServiceType {
+public enum GatewayType {
     /**
      * Customer Equipment Gateway
      */
@@ -34,36 +34,36 @@ public enum CloudServiceType {
     /**
      * Identifier value
      */
-    private final long id;
-    private final static Map<Long, CloudServiceType> ID_MAP;
+    private final int id;
+    private final static Map<Integer, GatewayType> ID_MAP;
 
     static {
-        CloudServiceType[] values = CloudServiceType.values();
+        GatewayType[] values = GatewayType.values();
         ID_MAP = new HashMap<>(values.length);
 
-        for (CloudServiceType v : values) {
+        for (GatewayType v : values) {
             ID_MAP.put(v.getId(), v);
         }
     }
 
-    CloudServiceType(long id) {
+    GatewayType(int id) {
         this.id = id;
     }
 
-    public long getId() {
+    public int getId() {
         return id;
     }
 
-    public static CloudServiceType getById(long id) {
+    public static GatewayType getById(int id) {
         return ID_MAP.get(id);
     }
 
     @JsonCreator
-    public static CloudServiceType getByName(String value) {
-        return JsonDeserializationUtils.deserializEnum(value, CloudServiceType.class, UNSUPPORTED);
+    public static GatewayType getByName(String value) {
+        return JsonDeserializationUtils.deserializEnum(value, GatewayType.class, UNSUPPORTED);
     }
 
-    public static boolean isUnsupported(CloudServiceType value) {
+    public static boolean isUnsupported(GatewayType value) {
         return UNSUPPORTED.equals(value);
     }
 }
