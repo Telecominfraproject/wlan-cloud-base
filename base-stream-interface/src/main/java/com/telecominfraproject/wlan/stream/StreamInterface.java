1730ffdcbff5f2af72537dd4770e342f909e48fb
Dmitry Toptygin <dmitry.toptygin@connectus.ai>
2020-06-03 11:30:55 -0400

    added bulk operations into StreamInterface

--- base-stream-interface/src/main/java/com/telecominfraproject/wlan/stream/StreamInterface.java
+++ base-stream-interface/src/main/java/com/telecominfraproject/wlan/stream/StreamInterface.java
@@ -1,11 +1,20 @@
 package com.telecominfraproject.wlan.stream;
 
+import java.util.List;
 
 /**
  * @author ekeddy
+ * @author dtop
  *
  */
 public interface StreamInterface<T> {
 
     void publish(T record);
+
+    default void publish(List<T> records) {
+    	if(records!=null) {
+    		records.forEach(r -> publish(r));
+    	}
+    }
+
 }
