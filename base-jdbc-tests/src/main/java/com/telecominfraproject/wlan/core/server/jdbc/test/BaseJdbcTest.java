e5d2e42751b5d6fc151078d42358a260a67fcec1
Dmitry Toptygin <dmitry.toptygin@connectus.ai>
2020-12-29 18:48:43 -0500

    alow rdbms-related unit tests from different maven modules to be combined to set up composite schemas

--- base-jdbc-tests/src/main/java/com/telecominfraproject/wlan/core/server/jdbc/test/BaseJdbcTest.java
+++ base-jdbc-tests/src/main/java/com/telecominfraproject/wlan/core/server/jdbc/test/BaseJdbcTest.java
@@ -1,18 +1,11 @@
 package com.telecominfraproject.wlan.core.server.jdbc.test;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
+import java.io.IOException;
+import java.lang.reflect.Field;
 
 import javax.sql.DataSource;
 
 import org.junit.runner.RunWith;
-import org.reflections.Reflections;
-import org.reflections.scanners.ResourcesScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,18 +15,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.transaction.AfterTransaction;
 import org.springframework.test.context.transaction.BeforeTransaction;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
-import com.telecominfraproject.wlan.core.model.json.BaseJsonModel;
 import com.telecominfraproject.wlan.core.server.jdbc.BaseJDbcDataSource;
 import com.telecominfraproject.wlan.core.server.jdbc.BaseKeyColumnConverter;
 import com.telecominfraproject.wlan.core.server.jdbc.KeyColumnUpperCaseConverter;
@@ -100,51 +94,68 @@ public abstract class BaseJdbcTest {
             // creates a HSQL in-memory db populated from scripts
             // classpath:schema-hsqldb-test.sql and classpath:test-data.sql
             // this will auto-wire DataSource object
-            EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder()
-                    .addScript("classpath:schema-hsqldb-test.sql");
+            EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+
+            // If the EmbeddedDatabaseBuilder class had a following method, we would have used it directly:
+            //            public EmbeddedDatabaseBuilder addScript(Resource scriptResource) {
+            //                this.databasePopulator.addScript(scriptResource);
+            //                return this;
+            //            }
+            // But it does not have this method, and we will have to use reflection to achieve similar effect.
+            //
+            // We need to expose ResourceDatabasePopulator databasePopulator field of the
+            // EmbeddedDatabaseBuilder to be able to pass to it resources with the same name
+            // in the classpath, i.e. schema-hsqldb-test.sql files that are defined in
+            // different modules
+            ResourceDatabasePopulator dbPopulator;
+            try {
+                Field dbPopulatorField = EmbeddedDatabaseBuilder.class.getDeclaredField("databasePopulator");
+                //the field is private, so we have to use a hammer
+                dbPopulatorField.setAccessible(true);
+                dbPopulator = (ResourceDatabasePopulator) dbPopulatorField.get(builder);
+            } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+                throw new RuntimeException("Cannot get access to dbPopulator of the EmbeddedDatabaseBuilder : " + e.getMessage(), e);
+            }  
+            
+            //
+            // Add the schema-hsqldb-test.sql scripts to the DB builder
+            //
+            try {
+                PathMatchingResourcePatternResolver pRes = new PathMatchingResourcePatternResolver();
+                Resource[] resources = pRes.getResources("classpath*:schema-hsqldb-test.sql");
+                if(resources!=null) {
+                    for(Resource r: resources) {
+                        dbPopulator.addScript(r);
+                        LOG.debug("Adding SQL script {} - {}", r.getFilename(), r.getURI());
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Cannot load SQL scripts: " + e.getMessage(), e);
+            }
+
 
             builder.generateUniqueName(true);
             
             //
-            // We only want to add the test-data.sql if the file actually
-            // exists.
+            // Now add the test-data.sql scripts to the DB builder
             //
-            Set<String> testDataFiles = getReflections().getResources(Pattern.compile("test-data.sql"));
-
-            if (!CollectionUtils.isEmpty(testDataFiles)) {
-                builder.addScript("classpath:test-data.sql");
+            try {
+                PathMatchingResourcePatternResolver pRes = new PathMatchingResourcePatternResolver();
+                Resource[] resources = pRes.getResources("classpath*:test-data.sql");
+                if(resources!=null) {
+                    for(Resource r: resources) {
+                        dbPopulator.addScript(r);
+                        LOG.debug("Adding SQL script {} - {}", r.getFilename(), r.getURI());
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Cannot load SQL scripts: ", e);
             }
+
 
             EmbeddedDatabase db = builder.build();
-            return new BaseJdbcTestDatabase(db, new KeyColumnUpperCaseConverter());
-        }
-
-        public static Reflections getReflections() {
-            //scan urls that contain 'com.telecominfraproject.wlan' and vendor-specific top level packages, use the ResourcesScanner
-
-            List<URL> urls =  new ArrayList<>();
-            urls.addAll(ClasspathHelper.forPackage("com.telecominfraproject.wlan"));
             
-            //add vendor packages
-            String vendorTopLevelPackages = BaseJsonModel.getVendorTopLevelPackages();
-
-            if(vendorTopLevelPackages!=null) {
-                String[] vendorPkgs = vendorTopLevelPackages.split(",");
-                for(int i=0; i< vendorPkgs.length; i++) {
-                    if(vendorPkgs[i].trim().isEmpty()) {
-                        continue;
-                    }
-                    
-                    urls.addAll(ClasspathHelper.forPackage(vendorPkgs[i]));
-                    
-                }
-            }
-                    
-            Reflections reflections =   new Reflections(new ConfigurationBuilder()
-                    .setUrls(urls)
-                    .setScanners(new ResourcesScanner() ));         
-
-            return reflections;
+            return new BaseJdbcTestDatabase(db, new KeyColumnUpperCaseConverter());
         }
 
         @Bean
