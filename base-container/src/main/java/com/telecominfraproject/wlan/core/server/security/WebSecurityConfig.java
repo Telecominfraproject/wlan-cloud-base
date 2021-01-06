d1723f7913239c0a80bbcb5d06cd3a17a40298e5
ralphlee <ralph.lee@netexperience.com>
2021-01-06 16:02:02 -0500

    [WIFI-1226] callbackUrl removed, updated pom

--- base-container/src/main/java/com/telecominfraproject/wlan/core/server/security/WebSecurityConfig.java
+++ base-container/src/main/java/com/telecominfraproject/wlan/core/server/security/WebSecurityConfig.java
@@ -878,14 +878,14 @@ public abstract class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             accessTypeValue = environment.getProperty("tip.wlan.auth0.accessType",
                     getDefaultAccessType(providerIndex));
             jwksLocation = environment.getProperty("tip.wlan.auth0.jwksLocation", DEFAULT_AUTH0_PROPERTY);
-            claimsUrl = environment.getProperty("tip.wlan.auth0.callbackUrl"); // See Auth0UserDetails
+            claimsUrl = environment.getProperty("tip.wlan.auth0.claimsUrl"); // See Auth0UserDetails
         } else {
             clientSecret = environment.getProperty("tip.wlan.auth0.clientSecret" + providerIndex);
             issuer = environment.getProperty("tip.wlan.auth0.issuerUri" + providerIndex);
             accessTypeValue = environment.getProperty("tip.wlan.auth0.accessType" + providerIndex,
                     getDefaultAccessType(providerIndex));
             jwksLocation = environment.getProperty("tip.wlan.auth0.jwksLocation" + providerIndex);
-            claimsUrl = environment.getProperty("tip.wlan.auth0.callbackUrl" + providerIndex);
+            claimsUrl = environment.getProperty("tip.wlan.auth0.claimsUrl" + providerIndex);
         }
         // Be default, use HS256 decoding which requires clientSecret
         if (null == clientSecret) {
